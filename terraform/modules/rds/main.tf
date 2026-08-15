0825bb528452fbcd109e2f84e0c08835b91d43fe
asd <asd@gmail.com>
2026-08-15 23:18:20 +0530

    new commit

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+
+      configuration_aliases = [
+        aws.dr
+      ]
+    }
+  }
+}
+
 # Security Group for RDS
 resource "aws_security_group" "rds" {
   name_prefix = "rds-"
