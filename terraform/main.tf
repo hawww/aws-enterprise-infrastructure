240df01d3dc48a243914d1bea89f22e7ef5c9a61
asd <asd@gmail.com>
2026-08-16 09:45:29 +0530

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,6 +96,59 @@ module "eks" {
   tags = local.common_tags
 }
 
+
+
+# Storage Class for EBS volumes
+resource "kubernetes_storage_class" "ebs_gp3" {
+  metadata {
+    name = "ebs-gp3"
+  }
+
+
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Delete"
+
+  parameters = {
+    type       = "gp3"
+    iops       = "3000"
+    throughput = "125"
+    encrypted  = "true"
+    kms_key_id = var.kms_key_arn
+  }
+
+  depends_on = [module.eks]
+}
+
+# Set default storage class
+resource "kubernetes_storage_class" "ebs_gp3_default" {
+  metadata {
+    name = "ebs-gp3-default"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Delete"
+
+  parameters = {
+    type       = "gp3"
+    iops       = "3000"
+    throughput = "125"
+    encrypted  = "true"
+    kms_key_id = var.kms_key_arn
+  }
+
+  depends_on = [kubernetes_storage_class.ebs_gp3]
+}
+
+
+
+
+
+
+
+
 # Module 5: RDS Multi-AZ Database
 module "rds" {
   source = "./modules/rds"
