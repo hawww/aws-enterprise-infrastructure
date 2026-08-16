33d1a6daae12621551b5674068d8a296a7814220
asd <asd@gmail.com>
2026-08-16 16:17:32 +0530

    added req providers

--- terraform/modules/addons/main.tf
+++ terraform/modules/addons/main.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.2"
     }
   }
 }
+
 
 
 resource "aws_eks_addon" "ebs_csi" {
