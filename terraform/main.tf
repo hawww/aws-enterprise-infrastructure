c5d670ff12f5cc50f57a08fee4bd855fced90f1f
asd <asd@gmail.com>
2026-08-16 10:00:03 +0530

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,7 +111,7 @@ resource "kubernetes_storage_class" "ebs_gp3" {
     iops       = "3000"
     throughput = "125"
     encrypted  = "true"
-    kms_key_id = var.kms_key_arn
+    kms_key_id = module.security.kms_key_arn
   }
 
   depends_on = [module.eks]
