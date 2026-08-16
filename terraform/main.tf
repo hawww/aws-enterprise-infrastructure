a1f031fe0794973ce9ffe574df62339e3c86fbbd
asd <asd@gmail.com>
2026-08-17 00:32:05 +0530

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,52 +128,6 @@ data "aws_eks_cluster_auth" "cluster" {
 
 
 
-# Storage Class for EBS volumes
-resource "kubernetes_storage_class" "ebs_gp3" {
-  metadata {
-    name = "ebs-gp3"
-  }
-
-
-  storage_provisioner = "ebs.csi.aws.com"
-  reclaim_policy      = "Delete"
-
-  parameters = {
-    type       = "gp3"
-    iops       = "3000"
-    throughput = "125"
-    encrypted  = "true"
-    kms_key_id = module.security.kms_key_arn
-  }
-
-  depends_on = [module.eks]
-}
-
-# Set default storage class
-resource "kubernetes_storage_class" "ebs_gp3_default" {
-  metadata {
-    name = "ebs-gp3-default"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
-
-  storage_provisioner = "ebs.csi.aws.com"
-  reclaim_policy      = "Delete"
-
-  parameters = {
-    type       = "gp3"
-    iops       = "3000"
-    throughput = "125"
-    encrypted  = "true"
-    kms_key_id = module.security.kms_key_arn
-  }
-
-  depends_on = [kubernetes_storage_class.ebs_gp3]
-}
-
-
-
 
 
 
@@ -234,158 +188,6 @@ resource "kubernetes_namespace" "default" {
   depends_on = [module.eks]
 }
 
-resource "kubernetes_manifest" "user_service" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-user.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    module.eks,
-    module.addons,
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "user_service1" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-user1.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    module.eks,
-    module.addons,
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "user_service2" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-user2.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    module.eks,
-    module.addons,
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "user_service3" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-user3.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    module.eks,
-    module.addons,
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "user_service4" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-user4.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    module.eks,
-    module.addons,
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "order_service" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-order.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    module.eks,
-    module.addons,
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "order_service1" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-order1.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "order_service2" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-order2.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "order_service3" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-order3.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-
-resource "kubernetes_manifest" "order_service4" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/microservice-order4.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks]
-
-  depends_on = [
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-resource "kubernetes_manifest" "ingress" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/ingress.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks, module.monitoring]
-
-  depends_on = [
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
-
-resource "kubernetes_manifest" "ingress1" {
-  #count    = var.deploy_k8s_resources ? 1 : 0
-  manifest = yamldecode(file("${path.module}/k8s-manifests/ingress1.yaml"))
-
-  #depends_on = [kubernetes_namespace.default, module.eks, module.monitoring]
-
-  depends_on = [
-    kubernetes_storage_class.ebs_gp3
-  ]
-}
 
 # Route53 for DNS and Failover
 resource "aws_route53_zone" "primary" {
@@ -490,3 +292,12 @@ resource "aws_route53_record" "app_alias" {
 
 
 
+
+# Module 3: IAM Roles and Service Accounts (IRSA)
+module "resources1" {
+  source = "./modules/resources1"
+
+  count = var.deploy_k8s_resources ? 1 : 0
+
+  #depends_on = [module.eks]
+}
