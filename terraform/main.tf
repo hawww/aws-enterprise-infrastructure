0b6161bad773526c35d61e11dc0b91fdbbf1d1f3
asd <asd@gmail.com>
2026-08-17 01:18:01 +0530

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,19 +23,23 @@ locals {
 }
 
 
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_endpoint
+}
+
 
 # Configure Kubernetes Provider
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
