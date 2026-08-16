24cfd212cedce17870043b7723d088202bf91a1d
asd <asd@gmail.com>
2026-08-16 10:27:34 +0530

    commit

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -3,6 +3,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.2.0"
 
+  enable_cluster_creator_admin_permissions = true
+
   cluster_name    = "${var.project_name}-eks-${var.environment}"
   cluster_version = var.cluster_version
 
