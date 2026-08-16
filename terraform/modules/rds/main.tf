dd7ed06ebd1010ca685cc3e3216dc49b3c50a450
asd <asd@gmail.com>
2026-08-17 04:49:19 +0530

    new 26

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -10,6 +10,35 @@ terraform {
   }
 }
 
+
+
+data "aws_eks_cluster" "this" {
+  name = "enterprise-eks-prod"
+}
+
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = "enterprise-eks-prod"
+}
+
+
+
+# Configure Kubernetes Provider
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 # Security Group for RDS
 resource "aws_security_group" "rds" {
   name_prefix = "rds-"
