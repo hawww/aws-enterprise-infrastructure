54062d618855bdce6acd673ac5a946b6228a88c8
asd <asd@gmail.com>
2026-08-17 04:59:21 +0530

    new 28

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -118,32 +118,6 @@ resource "aws_autoscaling_group_tag" "cluster_autoscaler_discovery" {
 #
 
 
-data "aws_eks_cluster" "this" {
-  name = "enterprise-eks-prod"
-}
-
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = "enterprise-eks-prod"
-}
-
-
-
-# Configure Kubernetes Provider
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
 #  set {
 #    name  = "clusterName"
 #    value = module.eks.cluster_name
