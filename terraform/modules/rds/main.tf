30b1b5a6cda04d7a115128d49bcb8320dfe83c06
asd <asd@gmail.com>
2026-08-16 10:22:42 +0530

    commit

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -77,7 +77,7 @@ module "rds" {
   backup_window                    = "03:00-04:00"
   maintenance_window               = "mon:04:00-mon:05:00"
   skip_final_snapshot              = false
-  final_snapshot_identifier_prefix = "${var.project_name}-db-${var.environment}-final-"
+  final_snapshot_identifier_prefix = "${var.project_name}-db-${var.environment}-final11"
 
   # Performance Insights
   performance_insights_enabled          = true
